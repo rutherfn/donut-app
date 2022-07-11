d690739711a18008bfb038e3d3612559b7b82ab2
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2022-07-11 14:45:12 -0500

    finish first version of the splash screen

--- code/HappyBirthday/app/src/main/java/com/example/happybirthday/SplashActivity.kt
+++ code/HappyBirthday/app/src/main/java/com/example/happybirthday/SplashActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.happybirthday.databinding.FragmentSplashBinding
 
-const val SPLASH_TIMER_DURATION = 4500L
-const val TYPEWRITER_CHARACTER_DELAY = 150L
+const val SPLASH_TIMER_DURATION = 6000L
+const val TYPEWRITER_CHARACTER_DELAY = 90L
+const val TYPEWRITER_SECOND_START_DURATION = 2880L
 
 class SplashActivity : AppCompatActivity() {
 
@@ -16,8 +19,13 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = FragmentSplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        main(binding = binding)
+    }
+
+    private fun main(binding: FragmentSplashBinding) {
         splashTimer()
-        animateLoadingText(binding = binding)
+        startDonutAnimation(binding = binding)
+        animateText(binding = binding)
     }
 
     private fun splashTimer() {
@@ -27,14 +35,30 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun startMainScreen() {
-        val splashActivity = this
-        val intent = Intent(splashActivity, MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-    private fun animateLoadingText(binding: FragmentSplashBinding) {
+    private fun startDonutAnimation(binding: FragmentSplashBinding) {
+        binding.ivDonut.animation = AnimationUtils.loadAnimation(this, R.anim.donut_rotate_animation)
+    }
+
+    private fun animateText(binding: FragmentSplashBinding) = animateFirstLoadingText(binding = binding)
+
+    private fun animateFirstLoadingText(binding: FragmentSplashBinding) {
         binding.twLoadingYourDonuts.setCharacterDelay(TYPEWRITER_CHARACTER_DELAY)
         binding.twLoadingYourDonuts.animateText(application.getText(R.string.loading_your_donuts))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            animateSecondLoadingText(binding = binding)
+        }, TYPEWRITER_SECOND_START_DURATION)
+    }
+
+    private fun animateSecondLoadingText(binding: FragmentSplashBinding) {
+        binding.twLoadingYourDonuts.visibility = View.GONE
+        binding.twLoadingYourDonuts2.visibility = View.VISIBLE
+        binding.twLoadingYourDonuts2.setCharacterDelay(TYPEWRITER_CHARACTER_DELAY)
+        binding.twLoadingYourDonuts2.animateText(application.getText(R.string.loading_your_donuts))
     }
 }
