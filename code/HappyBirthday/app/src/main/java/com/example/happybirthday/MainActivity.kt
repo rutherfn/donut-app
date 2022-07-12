7a5542071592aa69001b805976e78f1f369d6dff
Nicholas Rutherford <nrutherf@BRK-204081.local>
2022-07-12 16:47:10 -0500

    created base screens and navigation

--- code/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ code/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.happybirthday.databinding.ActivityMainBinding
+import com.example.happybirthday.donutmap.DonutMapFragment
+import com.example.happybirthday.feed.FeedFragment
+import com.example.happybirthday.rankyourdonuts.RankYourDonutsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +14,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding?.let { bind ->
-            setContentView(bind.root)
+        binding?.let { binding ->
+            setContentView(binding.root)
+            launchDonutMap()
+            listenToBottomNavigation(binding = binding)
         }
+    }
+
+    fun listenToBottomNavigation(binding: ActivityMainBinding) {
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.donutMap -> {
+                    launchDonutMap()
+                }
+                R.id.feed -> {
+                    launchFeed()
+                }
+                R.id.donutRankings -> {
+                    launchRankYourDonuts()
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    fun launchDonutMap() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcvMain, DonutMapFragment::class.java, null)
+            .commit()
+    }
+
+    fun launchFeed() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcvMain, FeedFragment::class.java, null)
+            .commit()
+    }
+
+    fun launchRankYourDonuts() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcvMain, RankYourDonutsFragment::class.java, null)
+            .commit()
     }
 }
