0e45098ca0ddee38d42cba741477143328bed0dd
AshleyWhite <ashley.white@milwaukeetool.com>
2022-07-15 14:35:30 -0500

    more map stuff

--- code/DonutApp/app/src/main/java/com/example/donutApp/donutmap/DonutMapFragment.kt
+++ code/DonutApp/app/src/main/java/com/example/donutApp/donutmap/DonutMapFragment.kt
@@ -1,16 +1,19 @@
 package com.example.donutApp.donutmap
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.donutApp.R
 import com.example.donutApp.databinding.FragmentDonutMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.OnMapReadyCallback
-import java.lang.Exception
+import com.google.android.gms.maps.model.LatLng
 
-class DonutMapFragment : Fragment(R.layout.fragment_donut_map), OnMapReadyCallback {
+class DonutMapFragment : Fragment(R.layout.fragment_donut_map) {
 
     private var binding: FragmentDonutMapBinding? = null
     private var maps: GoogleMap? = null
@@ -18,12 +21,27 @@ class DonutMapFragment : Fragment(R.layout.fragment_donut_map), OnMapReadyCallba
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentDonutMapBinding.bind(view)
         binding?.let { binding ->
-            initializeMap(binding = binding, savedInstanceState = savedInstanceState)
+            checkLocationPermission(binding = binding, savedInstanceState = savedInstanceState)
         }
     }
 
-    fun checkLocationPermission (binding: FragmentDonutMapBinding) {
+    fun checkLocationPermission(binding: FragmentDonutMapBinding, savedInstanceState: Bundle?) {
+        context?.let { context ->
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                initializeMap(binding, savedInstanceState)
+            } else {
+//                requestPermissions(
+//                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            }
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     fun initializeMap(binding: FragmentDonutMapBinding, savedInstanceState: Bundle?) {
@@ -31,20 +49,22 @@ class DonutMapFragment : Fragment(R.layout.fragment_donut_map), OnMapReadyCallba
         binding.mvDonutShopMap.getMapAsync {
             maps = it
             maps?.let { googleMap ->
+                println("gettt")
+                googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN)
                 googleMap.isMyLocationEnabled = true
                 googleMap.uiSettings.isMyLocationButtonEnabled = true
+                val latLng = LatLng(43.041069, -87.909416)
+                val yourLocation = CameraUpdateFactory.newLatLngZoom(latLng, 5f)
+                googleMap.animateCamera(yourLocation)
                 try {
                     context?.let { context ->
                         MapsInitializer.initialize(context)
+                        println("$googleMap.")
                     }
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }
         }
-    }
-
-    override fun onMapReady(p0: GoogleMap) {
-        println("on map ready")
     }
 }
